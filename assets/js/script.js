45325dcbc1727290723273919627818d94259ceb
sush3871 <sushanrshirke96@gmail.com>
2024-07-17 15:46:24 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    let currentIndex = 0;
+    const items = $('.diamond-links__item');
+    const itemCount = items.length;
+    let interval;
+
+    function activateNextItem() {
+        items.removeClass('active');
+        $(items[currentIndex]).addClass('active');
+        currentIndex = (currentIndex + 1) % itemCount;
+    }
+
+    function startInterval() {
+        interval = setInterval(activateNextItem, 2000);
+    }
+
+    function stopInterval() {
+        clearInterval(interval);
+    }
+
+    items.hover(function() {
+        stopInterval();
+        items.removeClass('active');
+        $(this).addClass('active');
+        currentIndex = items.index(this);
+    }, function() {
+        startInterval();
+    });
+
+    activateNextItem();
+    startInterval();
+});
