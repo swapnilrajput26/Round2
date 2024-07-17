c1683c61cecd143cea48377b76463605670e7ebb
Sushant Shirke <122607755+sush3871@users.noreply.github.com>
2024-07-17 18:47:09 +0530

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,3 +30,21 @@ $(document).ready(function() {
     activateNextItem();
     startInterval();
 });
+
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
